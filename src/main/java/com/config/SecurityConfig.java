e3fb15267fe3a77ba6d4ddde7d442128ce02b505
dh <hyunlove12@naver.com>
2020-02-18 23:47:20 +0900

    이그노어링

--- src/main/java/com/config/SecurityConfig.java
+++ src/main/java/com/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -14,6 +15,7 @@ import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
@@ -70,6 +72,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 				.mvcMatchers("/", "/info", "/account/**").permitAll()
 				.mvcMatchers("/admin").hasRole("ADMIN") // admin은 ADMIN권한 필요
 				.mvcMatchers("/user").hasRole("USER") // user권한
+				// .authorizeRequests()로 인하여 15개의 filter가 적용된다 -> anonymousAuthenticationfilter, filterSecurityInterceptor가 허용한다. 
+				// accessDesicionManager가 허용
+				// .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // ignoring과 결과는 똑같으나 과정이 다르다 -> 속도가 더 느림
 				.anyRequest().authenticated(); //그 외 어떠한 요청은 인증만 하면 된다.
 				//.expressionHandler(expressionHandler()); // expressionHandler를 override하는 방법
 				// .and()	//메소드 체이닝
@@ -87,4 +92,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		//userdetail구현체르 사용하여 
 		auth.userDetailsService(accountService);	//bean으로 드등록만 되어 있으면 사용 가능	
 	}
+	
+	// 필터 적용을 제외하고 싶을 때 websecurity설정
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// 매번 static 리소를 적어줘야
+		// web.ignoring().mvcMatchers("/favicon.ico");
+		// 리소스를 자동으로 시큐리티 무시
+		// .toStaticResources() -> at을 사용해서 구체적 명시 가능
+		// 동적인 리소스(/main)는 httpSecurity에서 설정해야한다 -> 특정 filter가 적용되야 하기 때문
+		web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+	}
+	
 }
